bf7b9f71d8362082be98d4f7ad815af4b3d56410
Mohammed Siddiqui <mohammad.siddiqui@tenpearls.com>
2018-07-30 20:29:21 +0500

    Some work Commit for testing out threads in Node.js

--- threads.js
+++ threads.js
@@ -1,8 +1,8 @@
-// process.env.UV_THREADPOOL_SIZE = 4;
+// process.env.UV_THREADPOOL_SIZE = 2;
 
 const crypto = require('crypto');
 
-let startTime = null;
+const iterations = 1;
 
 function expensiveOperation(instance, startTime) {
     crypto.pbkdf2('password', 'someSalt', 100000, 512, 'sha512', () => {
@@ -10,18 +10,22 @@ function expensiveOperation(instance, startTime) {
     });
 }
 
-function expensiveSyncOperation(instance, startTime) {
-    crypto.pbkdf2Sync('password', 'someSalt', 100000, 512, 'sha512');
-    console.log(`${instance}: `, Date.now() - startTime);
-}
-
-function multipleExpensiveOperations(num) {
-    startTime = Date.now();
-    for (let i = 0; i < num; i++) {
+function multipleExpensiveOperations(iter) {
+    let startTime = Date.now();
+    for (let i = 0; i < iter; i++) {
         expensiveOperation(i + 1, startTime);
     }
 }
 
-multipleExpensiveOperations(2);
+function init() {
+    multipleExpensiveOperations(iterations);
+}
+
+init();
+
+// function expensiveSyncOperation(instance, startTime) {
+//     crypto.pbkdf2Sync('password', 'someSalt', 100000, 512, 'sha512');
+//     console.log(`${instance}: `, Date.now() - startTime);
+// }
 
 
